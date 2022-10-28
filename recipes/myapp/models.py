27f145e143498718c703bf48c5f79ff67f88d57d
Nicholas Fell <sneah9@gmail.com>
2022-10-28 15:21:16 -0400

    ingredients table added to database

--- recipes/myapp/models.py
+++ recipes/myapp/models.py
@@ -5,3 +5,10 @@ class Recipe(models.Model):
     title = models.CharField(max_length=200)
     ingredients = models.CharField(max_length=200)
     pub_date = models.DateField('date published')
+    picture = models.ImageField('meal image')
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    amount = models.CharField(int=0)
+    unit = models.CharField(max_length=200)
--- recipes/myapp/views.py
+++ recipes/myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-from .models import Recipe
+from .models import * #(Recipe, Ingredient)
 
 def index(request):
     return HttpResponse("Hello, world!")
