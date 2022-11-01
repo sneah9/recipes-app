d355d18aae047ff08f4bb12a77246b80cfab3268
Nicholas Fell <sneah9@gmail.com>
2022-11-01 16:05:00 -0400

    updated relational table

--- recipes/myapp/admin.py
+++ recipes/myapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Recipe
+from .models import Recipe, Ingredient, Relationship
 admin.site.register(Recipe)
 
--- recipes/myapp/models.py
+++ recipes/myapp/models.py
@@ -5,10 +5,15 @@ class Recipe(models.Model):
     title = models.CharField(max_length=200)
     ingredients = models.CharField(max_length=200)
     pub_date = models.DateField('date published')
-    picture = models.ImageField(upload_to='meal images')
+    picture = models.ImageField(upload_to='recipe')
 
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=200)
-    amount = models.IntegerField()
+    amount = models.IntegerField(default=0)
     unit = models.CharField(max_length=200)
+
+
+class Relationship(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
