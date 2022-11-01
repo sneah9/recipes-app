86c32aba54a009be35a8ab3f08c16609a0fed49b
Nicholas Fell <sneah9@gmail.com>
2022-11-01 12:10:32 -0400

    updated tables

--- recipes/myapp/models.py
+++ recipes/myapp/models.py
@@ -5,10 +5,10 @@ class Recipe(models.Model):
     title = models.CharField(max_length=200)
     ingredients = models.CharField(max_length=200)
     pub_date = models.DateField('date published')
-    picture = models.ImageField('meal image')
+    picture = models.ImageField(upload_to='meal images')
 
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=200)
-    amount = models.CharField(int=0)
+    amount = models.IntegerField()
     unit = models.CharField(max_length=200)
