171c2927c5f5ac67c2f3022f02a9464857260148
Nicholas Fell <sneah9@gmail.com>
2022-11-01 16:18:22 -0400

    registered models

--- recipes/myapp/admin.py
+++ recipes/myapp/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Recipe, Ingredient, Relationship
+
 admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Relationship)
+
+
 
--- recipes/myapp/views.py
+++ recipes/myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-from .models import * #(Recipe, Ingredient)
+from .models import Recipe, Ingredient, Relationship
 
 def index(request):
     return HttpResponse("Hello, world!")
@@ -10,3 +10,7 @@ def index(request):
 def recipe_by_id(request, recipe_id):
     recipe = Recipe.objects.get(pk=recipe_id)
     return render(request, 'recipe_details.html', {'recipe':recipe})
+
+def ingredient_by_id(request, ingredient_id):
+    ingredient = Ingredient.objects.get(pk=ingredient_id)
+    return render(request, 'ingredient_details.html', {'ingredient':ingredient})
