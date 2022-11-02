59a9ab7aaa74cc3371da78122267799ddcb12e16
Nicholas Fell <sneah9@gmail.com>
2022-11-02 17:25:01 -0400

    more table editting

--- recipes/myapp/models.py
+++ recipes/myapp/models.py
@@ -5,7 +5,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=200)
     ingredients = models.CharField(max_length=200)
     pub_date = models.DateField('date published')
-    picture = models.ImageField(upload_to='recipe')
+    #picture = models.ImageField(upload_to='recipe', default=)
 
 
 class Ingredient(models.Model):
